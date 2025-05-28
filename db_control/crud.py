4d912f5327cbc67440979efc74a1a5f2e9ffe739
Yaoki Kobayashi <yaokikobayashi@YaokinoMacBook-Air.local>
2025-05-28 17:48:33 +0900

    データベースとCRUD操作の更新

--- db_control/crud.py
+++ db_control/crud.py
@@ -87,7 +87,7 @@ def myupdate(mymodel, values):
 
     customer_id = values.pop("customer_id")
 
-    query = "お見事！E0002の原因はこのクエリの実装ミスです。正しく実装しましょう"
+    query = update(mymodel).where(mymodel.customer_id == customer_id).values(values)
     try:
         # トランザクションを開始
         with session.begin():
